0ea563cfd9bdd1423545a1cb160af5340c278bf2
Mr. Fleming <efleming@stpatrick.org>
2018-04-10 08:35:34 -0500

    add to this file

---
+++ listTasks.js
@@ -0,0 +1,60 @@
+//Task 1
+function largest(list){
+
+}
+
+//Task 2
+function reverse(list){
+
+}
+
+//Task 3
+function cotains(list,e){
+
+}
+
+//Task 4
+function oddElements(list){
+
+}
+
+//Task 5
+function total(list){
+
+}
+
+//Task 6
+function isPalindrome(list){
+
+}
+
+//Task 7
+//Do second to last
+function recursiveTotal(list){
+
+}
+//Task 8
+function concatenate(listA, listB){
+
+}
+
+//Task 9
+function weave(listA, listB){
+
+}
+
+//Task 10
+//Attempt last
+function mergeInOrder(listA, listB){
+    
+}
+
+//Task 11
+function rotate(list,k){
+
+}
+
+//Task 12
+function fibonacci(){
+
+}
